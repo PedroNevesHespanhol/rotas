abe84781848b05ef4fa2942cd78d93a880ba01db
Pedro Neves <88465619+PedroNevesHespanhol@users.noreply.github.com>
2023-06-06 19:48:17 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Sobre from '../pages/Sobre';
-import Informacoes from '../pages/Informacoes';
+import Contato from '../pages/Informacoes';
 import Produtos from '../pages/Produtos';
 import Loja from '../pages/Loja';
 
@@ -18,7 +18,7 @@ function RoutesApp() {
                 <Route path='/sobre' element={ <Sobre/> } />
                 <Route path='/loja' element={ <Loja /> } />
                 <Route path='produtos/:id' element={ <Produtos /> } />
-                <Route path='informacoes/:id' element={ <Informacoes /> } />
+                <Route path='informacoes/contato' element={ <Contato /> } />
             </Routes>
         </BrowserRouter>
     );
